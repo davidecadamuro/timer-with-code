fcac692d3bdf03796baebcca467a3c3f89a0fce8
Davide Cadamuro <davide.cadamuro@gmail.com>
2024-07-21 21:36:00 +0000

    Working app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,20 @@
 import streamlit as st
 import time
 
-if 'key' not in st.session_state:
-    st.write("initialising state")
-    st.session_state.N = 5*60
-st.write(st.session_state)
-#N = 5*60
+@st.cache_resource
+def create_list():
+    l = [40] #[5*60]
+    return l
 
-with st.container():
-    st.title("💣💣💣 Es ist eine Bombe! 🧨🧨🧨")
+l = create_list()
+N = l[0]
 
-    st.write(
-        "Um den Countdown zu stoppen, geben Sie den Code ein."
-    )
+st.title("💣💣💣 Das ist eine Bombe! 🧨🧨🧨")
+
+st.write(
+    "Um den Countdown zu stoppen, geben Sie den Code ein."
+)
+with st.form("my_form"):
     col1, col2, col3, col4 = st.columns(4)
     with col1:
         n1 = st.number_input("", value=None, min_value=0, max_value=9, step=1, placeholder="", key="n1")
@@ -23,23 +25,32 @@ with st.container():
     with col4:
         n4 = st.number_input("", value=None, min_value=0, max_value=9, step=1, placeholder="", key="n4")
 
+    code = st.form_submit_button("Submit")
     #6459
-    if(n1 == 6 and n2 == 4 and n3 == 5 and n4 == 9):
-        st.write(
-            f"code is right {st.session_state.N}",
+    # pco = st.empty()
+    # if(n1 == 6 and n2 == 4 and n3 == 5 and n4 == 9):
+    #     pco.write(
+    #         f"code is right {l}",
 
-        )
+    #     )
+    # else:
+    #     pco.write(
+    #         f"code is wrong {l}"
+    #     )
+
+ph = st.empty()
+for secs in range(N, -1, -1):
+    if(secs == 0):
+        ph.header("💥💥💥💥BOOOOM!!!💥💥💥💥")
+        break
     else:
-        st.write(
-            f"code is wrong {st.session_state.N}"
-        )
-
-    ph = st.empty()
-    for secs in range(st.session_state.N, 0,-1):
-        #N = secs
-        st.session_state.N = secs
+        l[0] = secs
         mm, ss = secs//60, secs%60
-        ph.metric("Countdown", f"{mm:02d}:{ss:02d}, {st.session_state.N}")
-        time.sleep(1)
+        if(n1 == 6 and n2 == 4 and n3 == 5 and n4 == 9):
+            ph.metric("Countdown", f"{mm:02d}:{ss:02d} - {'Countdown gestoppt'}")
+            break
+        else:
+            ph.metric("Countdown", f"{mm:02d}:{ss:02d}")        
+            time.sleep(1)
 
     
